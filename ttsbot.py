187cbb121c6e3a23f6d97e1637e6d7b16e64375d
Anthony <amarucci13@gmail.com>
2017-05-26 22:39:13 -0400

    works now, filters out bot name

--- ttsbot.py
+++ ttsbot.py
@@ -9,7 +9,13 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['tts'])
 def handle_tts(message):
     try:
-        text = [x[5:] for x in message.text.split() if not any('@' in x)]
+        text = [x for x in message.text.split() if '/tts@' not in x]
+
+        #this will make sure that the string doesn't have any " in it where
+        #there shouldn't be, i.e. can't use this bot to execute commands
+        text = ' '.join(text) + "\""
+        text = "\"" + text[:text.index("\"")] + "\""  
+
         os.system("espeak -w speech.wav " + text)
 
         audio = open('speech.wav','rb')
